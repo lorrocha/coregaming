df1dbcbc90643a96dd78a576fc63043e61dd7037
Lorry Rocha <sorai120@yahoo.com>
2014-02-22 13:56:17 -0500

    Added a few unit tests for the event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :date
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:blanks) {['',nil]}
+
+  it {should have_valid(:name).when('Bill', 'Frank')}
+  it {should_not have_valid(:name).when(*blanks)}
+  it {should have_valid(:description).when('Bill', 'Frank')}
+  it {should_not have_valid(:description).when(*blanks)}
+  it {should have_valid(:date).when('Bill@bill.co', 'Frank1something@something.com')}
+  it {should_not have_valid(:date).when(*blanks)}
+
 end
