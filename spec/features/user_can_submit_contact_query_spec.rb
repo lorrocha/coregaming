08f75a45428a13c5c7d0923fb246d9fb29f9d46f
Lorry Rocha <sorai120@yahoo.com>
2014-02-22 14:00:36 -0500

    Quick maintenance of a feature test - the button was renamed

--- spec/features/user_can_submit_contact_query_spec.rb
+++ spec/features/user_can_submit_contact_query_spec.rb
@@ -12,7 +12,7 @@ feature 'A user can submit a contact us feedback form' do
     fill_in "Email", with: 'L@arabia.com'
     fill_in "Subject", with: 'Too much sand'
     fill_in "Description", with: 'There is too much sand in my shoes. Help.'
-    click_button "Submit Contact"
+    click_button "Hit us up!"
 
     expect(Contact.all.count).to eql(1)
     expect(page).to have_content('Your contact has been submitted! You should hear back shortly.')
@@ -24,7 +24,7 @@ feature 'A user can submit a contact us feedback form' do
 
   scenario 'A user cannot submit a contact inquiry if they dont fill in values' do
     visit new_contact_path
-    click_button "Submit Contact"
+    click_button "Hit us up!"
 
     expect(page).to have_content("can't be blank")
   end
