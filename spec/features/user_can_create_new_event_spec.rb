4801ac6f276b945d533cc11b16a838e005135fdb
Lorry Rocha <sorai120@yahoo.com>
2014-02-22 14:39:13 -0500

    Added a few more acceptance tests for the creation of the event page

--- spec/features/user_can_create_new_event_spec.rb
+++ spec/features/user_can_create_new_event_spec.rb
@@ -2,18 +2,39 @@ require 'spec_helper'
 
 feature 'An administrative user can create a new event' do
 
+  let(:thename) {'Super Shaq Sale'}
+  let(:thedesc) {'This is the best most Shaqiest sale ever'}
+  let(:thedate) {'Feb 3rd at 4pm'}
+
   scenario 'User can submit the event with everyone filled in' do
     visit new_event_path
 
-    thename = 'Super Shaq Sale'
-
     fill_in 'Name', with: thename
-    fill_in 'Description', with: 'This is the best most Shaqiest sale ever'
-    fill_in 'Date', with: 'Feb 3rd at 4pm'
+    fill_in 'Description', with: thedesc
+    fill_in 'Date', with: thedate
     fill_in 'Location', with: 'Core Gaming'
     click_button 'Create Event'
 
     expect(page).to have_content(thename)
+  end
+
+  scenario 'Location will always default to Core Gaming' do
+    visit new_event_path
+
+    fill_in 'Name', with: thename
+    fill_in 'Description', with: thedesc
+    fill_in 'Date', with: thedate
+
+    click_button 'Create Event'
+
+    expect(page).to have_content(thename)
+  end
+
+  scenario 'Event is not created if missing fields' do
+    visit new_event_path
+
+    click_button 'Create Event'
+    expect(page).to have_content("can't be blank")
   end
 
 end
